70cd5769c7c0069a8ccdc2938b1e865b2c32b9bd
Youssef Nader <55225729+youssef235@users.noreply.github.com>
2025-10-02 04:47:14 +0300

    Update settings.py

--- tg_hub/settings.py
+++ tg_hub/settings.py
@@ -148,11 +148,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Required for collectstatic
 
 # Media (uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
